15fd1312e069d8c52c7cf9580197ae1e55232d7c
gmanriqe <gmanriqe@gmail.com>
2020-04-30 14:16:22 -0500

    Api listar marcas y crear una marca

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.use(require('./usuario'));
 app.use(require('./login'));
+app.use(require('./usuario'));
+app.use(require('./marca'));
 app.use(require('./categoria'));
 app.use(require('./producto'));
 
---
+++ server/routes/marca.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const Marca = require('../models/marca.js');
+
+const app = express();
+
+/**
+ * Listar marcas
+ */
+app.get('/marca', (req, res) => {
+    Marca.find({})
+         .exec( (err, marcas) => {
+            if(err){
+                res.status(400).json({
+                    ok: false,
+                    err: err
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                marcas: marcas
+            });
+        });
+});
+
+/**
+ * Crear una marca
+ */
+app.post('/productos', verificaToken, (req, res) => {
+    let marca = new Marca({
+        nombre: req.body.nombre
+    });
+
+    marca.save( (err, marcaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err: err
+            });
+        }
+
+        if (!marcaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            marca: marcaDB
+        });
+    });
+});
+
+module.exports = app;
